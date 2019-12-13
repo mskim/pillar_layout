cf0ccbf14667609c0166a21275930bf9862c0452
Min Soo Kim <mskimsid@gmail.com>
2019-12-13 13:10:45 +0900

    fix wire_image path

--- app/models/reporter_image.rb
+++ app/models/reporter_image.rb
@@ -42,7 +42,7 @@ class ReporterImage < ApplicationRecord
   def source_path
     return unless wire_pictures
     # full_size = wire_pictures.split(" ").first
-    @filename_date = wire_pictures.split(".").first.scan(/\d{3,8}/).first
+    filename_date = wire_pictures.split(".").first.scan(/\d{3,8}/).first
     i = ReporterImage.find(id)
     # if i.source == "YNA"
     #   "/wire_source/201_PHOTO_YNA/#{@filename_date}"
@@ -51,15 +51,19 @@ class ReporterImage < ApplicationRecord
     # else
     #   "/wire_source/205_PHOTO_FR_YNA/#{@filename_date}"
     # end
-    @filename_code = wire_pictures.split(".").first.scan(/\d{4,4}/).last
-    if @filename_code == "0001" || @filename_code == "0007" || @filename_code == "0006" 
-      "/wire_source/201_PHOTO_YNA/#{@filename_date}"
-    elsif @filename_code == "0440" 
-      "/wire_source/203_GRAPHIC/#{@filename_date}"
-    elsif @filename_code == "0184" 
-      "/wire_source/205_PHOTO_FR_YNA/#{@filename_date}"
+    year_dir = filename_date.scan(/\d{4}/).first
+    month_dir = filename_date.scan(/\d{4}/).last.scan(/\d{2}/).first
+    day_dir = filename_date.scan(/\d{4}/).last.scan(/\d{2}/).last
+
+    filename_code = wire_pictures.split(".").first.scan(/\d{4,4}/).last
+    if filename_code == "0001" || filename_code == "0007" || filename_code == "0006" 
+      "/wire_source/201_PHOTO_YNA/#{year_dir}/#{month_dir}/#{day_dir}"
+    elsif filename_code == "0440" 
+      "/wire_source/203_GRAPHIC/#{year_dir}/#{month_dir}/#{day_dir}"
+    elsif filename_code == "0184" 
+      "/wire_source/205_PHOTO_FR_YNA/#{year_dir}/#{month_dir}/#{day_dir}"
     else
-      "/wire_source/202_PHOTO_TR/#{@filename_date}"
+      "/wire_source/202_PHOTO_TR/#{year_dir}/#{month_dir}/#{day_dir}"
     end
     # "/wire_source/201_PHOTO_YNA/"
   end
@@ -67,19 +71,19 @@ class ReporterImage < ApplicationRecord
   def full_size_path
     return unless wire_pictures
     full_size = wire_pictures.split(" ").first
-    source_path + "/#{full_size}"
+    source_path + "/full/#{full_size}"
   end
 
   def preview_path
     return unless wire_pictures
     preview = wire_pictures.split(" ")[1]
-    source_path + "/#{preview}"
+    source_path + "/pre/#{preview}"
   end
 
   def thumb_path
     return unless wire_pictures
     thumb = wire_pictures.split(" ").last
-    source_path + "/#{thumb}"
+    source_path + "/thumb/#{thumb}"
   end
   
 end
