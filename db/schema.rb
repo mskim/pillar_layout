70613d562e844441acc282d81958538caed764b0
naeil <naeil@naeilui-MacBookPro.local>
2020-03-25 10:24:16 +0900

    20200325 패치사항 v4
    1. 충돌병합 중...

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_03_20_053549) do
-=======
 ActiveRecord::Schema.define(version: 2020_03_22_224015) do
->>>>>>> upstream/master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
