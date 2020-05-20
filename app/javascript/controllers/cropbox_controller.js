1d2a8d214fcf73d0f549bef9a4465ea82ce1448c
naeil <naeil@naeilui-MacBookPro.local>
2020-05-20 11:30:05 +0900

    20200520 패치사항 v1
    1. 기사종류는 "사진"이면, working_article의 종류, 사이즈 정보를 crop.html.erb에 data 속성을 추가했습니다.
    그런 다음에 자바스크립트 파일에 if 조건부를 달아서 작성했습니다.
    ex) kind == "사진"이면 다음 코딩을 실행합니다.

--- app/javascript/controllers/cropbox_controller.js
+++ app/javascript/controllers/cropbox_controller.js
@@ -18,31 +18,48 @@ export default class extends Controller {
   ];
 
   initialize() {
+    Cropper.noConflict();
     this.cropbox();
   }
 
   connect() {}
 
   cropbox() {
-    // 이미지
+    // 이미지 타겟 설정
     const img = this.originImgTarget;
+    // 초기 설정
+    const lengthPerColumn = 171.496062992123;
+    const lengthPerRow = 97.32283464566807;
+    const lengthPerLine = 13.903262092238295;
     // 실시간 데이터 감시
     const watchCropX = this.watchCropXTarget;
     const watchCropY = this.watchCropYTarget;
     const watchCropW = this.watchCropWTarget;
     const watchCropH = this.watchCropHTarget;
+    // 저장된 data 속성 정보
+    const getDataArticleKind = this.data.get("article-kind");
+    const getDataArticleColumn = parseInt(this.data.get("article-column"));
+    const getDataArticleRow = parseInt(this.data.get("article-row"));
+    const getDataArticleLine = parseInt(this.data.get("article-line"));
     const getDataColumn = parseInt(this.data.get("column"));
     const getDataRow = parseInt(this.data.get("row"));
     const getDataLine = parseInt(this.data.get("extra-height-in-lines"));
-    // 계산된 크롭박스 정보
-    const computedWidth = getDataColumn * 171.496062992123;
-    const computedHeight =
-      getDataRow * 97.32283464566807 + getDataLine * 13.903262092238295;
-    // 저장된 크롭박스 정보
+    // 저장된 DB의 크롭박스 정보
     const savedDataCropX = parseInt(this.savedDataCropXTarget.innerText);
     const savedDataCropY = parseInt(this.savedDataCropYTarget.innerText);
     const savedDataCropW = parseInt(this.savedDataCropWTarget.innerText);
     const savedDataCropH = parseInt(this.savedDataCropHTarget.innerText);
+    // 계산된 크롭박스 정보
+    if (getDataArticleKind == "사진") {
+      var computedWidth = getDataArticleColumn * lengthPerColumn;
+      var computedHeight =
+        getDataArticleRow * lengthPerRow + getDataArticleLine * lengthPerLine;
+    } else {
+      var computedWidth = getDataColumn * lengthPerColumn;
+      var computedHeight =
+        getDataRow * lengthPerRow + getDataLine * lengthPerLine;
+    }
+    // 크롭 기능
     new Cropper(img, {
       viewMode: 2,
       dragMode: "move",
@@ -69,6 +86,5 @@ export default class extends Controller {
           (watchCropH.value = Math.round(event.detail.height));
       },
     });
-    console.log(getDataLine);
   }
 }
