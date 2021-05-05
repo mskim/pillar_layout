0fd8010f7d1b8bf35350493541851c853dcbb78f
Min Soo Kim <mskimsid@gmail.com>
2021-05-05 10:20:59 +0900

    clear gitignore cashe

--- public/1/page_library/21/5단통/2/ad/layout.rb
+++
@@ -1,4 +0,0 @@
-RLayout::NewsAdBox.new(is_ad_box: true, column: 6, row: 5, grid_width: 171.49606299212363, grid_height: 97.32283464566795, on_left_edge: true, on_right_edge: true, top_position: false, page_heading_margin_in_lines: 0, :article_line_draw_sides=>"[0, 0, 0, 0]") do
-  image(image_path: '/Users/mskim/Development/pillar_layout/storage/t2/ul/t2ul4htyo1qr4qilmwbprty9u9ss', fit_type: 4, layout_expand: [:width, :height])
-  relayout!
-end
